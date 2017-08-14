cccc0f8e5042e0114dbbaf09f53acad65d5f10b6
unknown <rosiek.kuba@gmail.com>
2017-08-14 14:55:46 +0100

    do sprawdzenia

---
+++ scripts.js
@@ -0,0 +1,15 @@
+$(function(){	
+function Phone(brand, price, color) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+}
+	var Samsung Galaxy S6 = new Phone("Samsung", 1250, "black");
+	var iPhone6S = new Phone("Apple", 2250, "silver");
+	var OnePlus One = new Phone("OnePlus", 1250, "gold");
+
+		Phone.prototype.printInfo = function() {
+			console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+}
+iPhone6S.printInfo();
+});
