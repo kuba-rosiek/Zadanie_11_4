e361c7639b7b0e6cbf37d374bc4b2b4d0e8ad169
kuba-rosiek <rosiek.kuba@gmail.com>
2017-08-18 09:30:08 +0100

    Dziala

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
-$(function(){	
 
 function Phone(brand, price, color) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
-	
+}
 	var SamsungGalaxyS6 = new Phone("Samsung", 1250, "black");
 	var iPhone6S = new Phone("Apple", 2250, "silver");
 	var OnePlusOne = new Phone("OnePlus", 1250, "gold");
@@ -12,6 +11,8 @@ function Phone(brand, price, color) {
 		Phone.prototype.printInfo = function() {
 			console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 		}		
-			iPhone6S.printInfo();			
-}
-});
+iPhone6S.printInfo();		
+SamsungGalaxyS6.printInfo();
+OnePlusOne.printInfo();					
+
+
